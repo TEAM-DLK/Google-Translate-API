fbb5889a21d6b5ee3b0d66a98e15757ea1f890c2
達南佳亞 <93440107+TEAM-DLK@users.noreply.github.com>
2025-02-09 04:44:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,3 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 OWNER_ID = os.getenv("OWNER_ID")
 GROUP_LINK = os.getenv("GROUP_LINK")
 CHANNEL_LINK = os.getenv("CHANNEL_LINK")
-
-# Add any other configuration or constants here
