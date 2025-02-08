68a2057975ed64c49260cce2fdc846881809eb0a
達南佳亞 <93440107+TEAM-DLK@users.noreply.github.com>
2025-02-09 05:08:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
 load_dotenv()
 
-# Bot configuration settings
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-OWNER_ID = os.getenv("OWNER_ID")
-GROUP_LINK = os.getenv("GROUP_LINK")
-CHANNEL_LINK = os.getenv("CHANNEL_LINK")
+# Bot configuration
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+OWNER_ID = os.getenv('OWNER_ID')
+CHANNEL_LINK = os.getenv('CHANNEL_LINK')
+GROUP_LINK = os.getenv('GROUP_LINK')
+
+# Scheduler settings
+SCHEDULED_TIME_MORNING = "12:00"
+SCHEDULED_TIME_NIGHT = "22:00"
