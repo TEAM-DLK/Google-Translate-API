2ab157920eb05197e81be04cfba76b9927a50389
DHANANJAYA <93440107+TEAM-DLK@users.noreply.github.com>
2025-02-13 17:19:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
 OWNER_ID = os.environ.get('OWNER_ID', '5917900136')
-CHANNEL_LINK = os.environ.get('CHANNEL_LINK', 'https://t.me/DOOZY_OFF')
+CHANNEL_LINK = os.environ.get('CHANNEL_LINK', 'https://t.me/DLKDevelopers')
 GROUP_LINK = os.environ.get('GROUP_LINK', 'https://t.me/DevDLK')
 REPO_LINK = os.environ.get('REPO_LINK', 'https://github.com/TEAM-DLK/Google-Translate-API')
